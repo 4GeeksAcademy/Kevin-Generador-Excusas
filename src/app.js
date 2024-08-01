e163f9899a09872225f307fe1e8c728be5725077
Kevin Villafuerte Montero <116960904+Kevin962022@users.noreply.github.com>
2024-08-01 15:56:09 +0000

    excuseando

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+//Pantalla de inicializacion
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = generadorExcusas();
 };
+//Funcion para generar las excusas
+const generadorExcusas = () => {
+  //declaracion de variables
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const WhatIndex = Math.floor(Math.random() * what.length);
+  const WhenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[WhatIndex] +
+    " " +
+    when[WhenIndex]
+  );
+};
+console.log("Excuse Generator");
+console.log(generadorExcusas());
